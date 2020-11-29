aa125a3c57ef7ac4a8328e20b32b1f7492136d3b
Paweł Trawiński <p.trawinski@xcitstudio.com>
2020-11-29 15:04:30 +0100

    added types of articles

--- src/js/cms-preview-templates/article_health.js
+++ src/js/cms-preview-templates/article_health.js
@@ -1,7 +1,7 @@
 import React from "react";
 import format from "date-fns/format";
 
-export default class ArticleEnvironment extends React.Component {
+export default class ArticleHealth extends React.Component {
   render() {
     const {entry, widgetFor, getAsset} = this.props;
     let image = getAsset(entry.getIn(["data", "image"]));
--- src/js/cms.js
+++ src/js/cms.js
@@ -5,13 +5,19 @@ import CMS from "netlify-cms-app";
 import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.css";
 
 import HomePreview from "./cms-preview-templates/home";
+import ArticleCulture from "./cms-preview-templates/article_culture";
 import ArticleEnvironment from "./cms-preview-templates/article_environment";
+import ArticleTechnology from "./cms-preview-templates/article_technology";
+import ArticleHealth from "./cms-preview-templates/article_health";
 import PostPreview from "./cms-preview-templates/post";
 import ContactPreview from "./cms-preview-templates/contact";
 
 CMS.registerPreviewStyle(styles, { raw: true });
 CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
-CMS.registerPreviewTemplate("articles_environment", ArticleEnvironment);
+CMS.registerPreviewTemplate("article_culture", ArticleCulture);
+CMS.registerPreviewTemplate("article_environment", ArticleEnvironment);
+CMS.registerPreviewTemplate("article_technology", ArticleTechnology);
+CMS.registerPreviewTemplate("article_health", ArticleHealth);
 CMS.registerPreviewTemplate("contact", ContactPreview);
 CMS.init();
