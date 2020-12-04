e5f3d3e3d957e59a6270511079b6be42896f6e41
Paweł Trawiński <p.trawinski@xcitstudio.com>
2020-12-04 19:47:35 +0100

    quiz score update

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -3,18 +3,36 @@ document.getElementById("check_answer").addEventListener("click", checkAnswers)
 function checkAnswers()
 {
     let questions = document.getElementById("questions").children;
+    let numberOfCorrect = 0;
+    let numberOfQuestions = questions.length;
 
-    for (let i = 0; i < questions.length; i++) {
-        console.log(checkQuestion(questions[i]));
+    for (let i = 0; i < numberOfQuestions; i++) {
+        if (checkQuestion(questions[i]))
+        {
+            numberOfCorrect++;
+        }
+
     }
+
+
+
+    showScore(numberOfQuestions, numberOfCorrect);
+
+}
+
+
+function showScore(maximum, scored) {
+    document.getElementById("score").innerHTML = "You scored: " +  scored + "/" + maximum + " points!" ;
+    document.getElementById("result_container").setAttribute("class", "");
 }
 
 function checkQuestion(questionReference)
 {
+
     let answers = questionReference.children[2];
     let answerNumber = questionReference.getAttribute("correct-answer");
-
-    let index = 0;
+    answers = answers.getElementsByTagName('input');
+    let index = -1;
 
     for (let i = 0; i < answers.length; i++) {
         if (answers[i].checked) {
