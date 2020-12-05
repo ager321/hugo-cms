3c999a0331192456fa85cf0063d44bdd07752ff6
Paweł Trawiński <p.trawinski@xcitstudio.com>
2020-12-05 21:40:18 +0100

    redirects

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ if (window.netlifyIdentity) {
   window.netlifyIdentity.on("init", (user) => {
     if (!user) {
       window.netlifyIdentity.on("login", () => {
+        console.log(user);
         document.location.href = "/admin/";
       });
     }
