0d52043be7212399a8e7af7ff9153454a202fa0c
Paweł Trawiński <p.trawinski@xcitstudio.com>
2020-12-07 19:05:56 +0100

    main page

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -30,6 +30,25 @@ export default class PostPreview extends React.Component {
         </div>
 
 
+        <div className="bg-grey-1 pv4">
+            <div className="ph3 mw7 center">
+
+                <div className="flex-l mhn2-l">
+                    <div className="w-40-l ph2-l">
+                        <h2 className="f2 b lh-title mb2">{entry.getIn(["data", "short_tech", "title"])}</h2>
+
+                        <p>{entry.getIn(["data", "short_tech", "text"])}</p>
+                    </div>
+
+                    <div className="w-60-l ph2-l">
+                        <img src='{entry.getIn(["data", "short_tech", "image"])}' alt="" className="mb3">
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
     </div>
 
   }
